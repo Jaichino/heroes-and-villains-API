c71b98732b5114ec144c7801ca64933e23277750
Juan <aichinojuani@gmail.com>
2025-07-11 13:03:31 -0300

    docs: add api metadata for documentation

--- app/main.py
+++ app/main.py
@@ -13,7 +13,43 @@ from app.routers import characters, powers, character_power
 
 ###################################################################################################
 # API configuration
-app = FastAPI()
+description = """
+This API allows you to do the following things:
+
+### Characters
+
+You will be able to:
+
+* **Create new characters**: You can create heroes and villains
+* **Read existing characters**: You can get all the characters or only one of them
+* **Update character's properties**: You can update any of the character's attributes
+* **Delete characters**: You can also delete a character
+
+### Powers
+
+This section allows you to:
+
+* **Create new powers**: You can create new powers with their respective damages
+* **Read powers**: You can get all the powers or only one.
+* **Update powers**: You can update any of the power's attributes
+* **Delete powers**: You can delete powers
+
+### Character's Powers
+
+And here, you can:
+
+* **Asign a power** to a character
+* **Read the powers** of a certain character
+* **Delete a power** of a certain character
+"""
+
+app = FastAPI(
+    title="Heroes and Villains API",
+    summary="RESTful API built with FastAPI for managing heroes and villains",
+    description=description,
+    contact={"name": "Aichino, Juan", "email": "aichinojuani@gmail.com"}
+)
+
 ###################################################################################################
 
 
